f85c145cd04b9ca2e3b9ea55bc52f04ffe4cd197
anand <anand.panchdhari@gmail.com>
2024-11-07 20:22:06 +0530

    Slide up in MainActivity.java will slide down on click

--- app/src/main/java/com/example/workoutapp2/MainActivity.java
+++ app/src/main/java/com/example/workoutapp2/MainActivity.java
@@ -122,6 +122,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                slideDown();
+            }
+        });
     }
 
     void slideUp() {
